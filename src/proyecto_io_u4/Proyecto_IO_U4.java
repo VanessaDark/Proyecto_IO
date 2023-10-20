01e36744816af82d62f3eb51df632bf79bb73f3a
DarkLigihtt <vancraft04@gmail.com>
2023-10-20 12:28:03 -0600

    Nuevas actualizaciones

---
+++ src/proyecto_io_u4/Proyecto_IO_U4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_io_u4;
+
+/**
+ *
+ * @author vanes
+ */
+public class Proyecto_IO_U4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
